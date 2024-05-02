a8d19236b8fb65ab6a433edf6c35ecb344ca3f80
AlbertSch8 <albert.schurrer@gmail.com>
2024-05-02 18:41:21 +0200

    přidání třídy Veci

--- src/Veci.java
+++ src/Veci.java
@@ -1,2 +1,36 @@
-public class Veci {
+public class Veci extends Herniobjekt {
+    private boolean jeSebratelny;
+    private int cisloMistnosti;
+    private boolean jeUkoncujici;
+
+    public Veci(String jmeno, String popis, boolean jeSebratelny, int cisloMistnosti, boolean jeUkocujici) {
+        super(jmeno, popis);
+        this.jeSebratelny = jeSebratelny;
+        this.cisloMistnosti = cisloMistnosti;
+        this.jeUkoncujici = jeUkocujici;
+    }
+
+    public boolean isJeSebratelny() {
+        return jeSebratelny;
+    }
+
+    public void setJeSebratelny(boolean jeSebratelny) {
+        this.jeSebratelny = jeSebratelny;
+    }
+
+    public int getCisloMistnosti() {
+        return cisloMistnosti;
+    }
+
+    public void setCisloMistnosti(int cisloMistnosti) {
+        this.cisloMistnosti = cisloMistnosti;
+    }
+    public boolean isUkoncujici() {
+        return jeUkoncujici;
+    }
+
+    public void setUkoncujici (boolean jeUkoncujici) {
+        this.jeUkoncujici = jeUkoncujici;
+    }
 }
+
