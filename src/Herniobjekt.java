fabc460d6302d732a3ccd78aee8989364d55ae25
AlbertSch8 <albert.schurrer@gmail.com>
2024-05-02 18:40:09 +0200

    přidání třídy Herniobjekt

---
+++ src/Herniobjekt.java
@@ -0,0 +1,27 @@
+public class Herniobjekt {
+    private String jmeno;
+
+    private String popis;
+
+    public Herniobjekt(String jmeno, String popis) {
+        this.jmeno = jmeno;
+        this.popis = popis;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public String getPopis() {
+        return popis;
+    }
+
+    public void setPopis(String popis) {
+        this.popis = popis;
+    }
+}
+
