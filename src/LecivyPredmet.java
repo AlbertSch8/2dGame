847a517de9fd1c390be59f739ff09c00ff6f8766
AlbertSch8 <albert.schurrer@gmail.com>
2024-05-09 12:44:03 +0200

    přidání třídy StitPredmet

--- src/LecivyPredmet.java
+++ src/LecivyPredmet.java
@@ -1,2 +1,17 @@
-public class LecivyPredmet {
+public class LecivyPredmet extends Veci{
+
+    private int leciveBody;
+
+    public LecivyPredmet(String jmeno, String popis, boolean jeSebratelny, int cisloMistnosti, boolean jeUkoncujici, int hmotnost, int leciveBody) throws Exception {
+        super(jmeno, popis, jeSebratelny, cisloMistnosti, jeUkoncujici, hmotnost);
+        this.leciveBody = leciveBody;
+    }
+
+    public int getLeciveBody() {
+        return leciveBody;
+    }
+
+    public void lecit(Hrac hrac) {
+        hrac.pridatZivoty(leciveBody);
+    }
 }
