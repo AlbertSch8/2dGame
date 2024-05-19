3a4d139e6c3d050c67d17138708b93a0f0414443
AlbertSch8 <albert.schurrer@gmail.com>
2024-05-19 13:35:18 +0200

    pridani promenne do tridy Pritel

--- src/Pritel.java
+++ src/Pritel.java
@@ -5,12 +5,14 @@ public class Pritel extends Herniobjekt{
     private boolean jeNepritel;
 
     private int cisloMistnosti;
+    private int zivoty;
 
-    public Pritel(String jmeno, String popis, boolean jePratelsky, boolean jeNepritel, int cisloMistnosti) {
+    public Pritel(String jmeno, String popis, boolean jePratelsky, boolean jeNepritel, int cisloMistnosti, int zivoty) {
         super(jmeno, popis);
         this.jePratelsky = jePratelsky;
         this.jeNepritel = jeNepritel;
         this.cisloMistnosti = cisloMistnosti;
+        this.zivoty = zivoty;
     }
 
     public boolean isJePratelsky() {
@@ -35,5 +37,13 @@ public class Pritel extends Herniobjekt{
 
     public void setCisloMistnosti(int cisloMistnosti) {
         this.cisloMistnosti = cisloMistnosti;
+    }
+
+    public int getZivoty() {
+        return zivoty;
+    }
+
+    public void setZivoty(int zivoty) {
+        this.zivoty = zivoty;
     }
 }
