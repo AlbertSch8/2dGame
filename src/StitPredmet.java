55aed287e87f08f82a3be3547a72eb26897029de
AlbertSch8 <albert.schurrer@gmail.com>
2024-05-29 11:31:02 +0200

    upraveni tridy StitPredmet

--- src/StitPredmet.java
+++ src/StitPredmet.java
@@ -1,10 +1,11 @@
-public class StitPredmet extends Veci{
+
+public class StitPredmet extends Veci {
 
     private double stitEfektivita;
 
-    public StitPredmet(String jmeno, String popis, boolean jeSebratelny, int cisloMistnosti, int hmotnost, double stitEfektivita) throws Exception {
-        super(jmeno, popis, jeSebratelny, cisloMistnosti, hmotnost);
-        this.stitEfektivita = stitEfektivita;
+    public StitPredmet(String jmeno, String popis, boolean jeSebratelny, int cisloMistnosti, int hmotnost) throws Exception {
+        super(jmeno, popis, jeSebratelny, cisloMistnosti, hmotnost,50);
+        this.stitEfektivita = 50.0;
     }
 
     public double getStitEfektivita() {
@@ -12,7 +13,7 @@ public class StitPredmet extends Veci{
     }
 
     public void aktivovatStit(Hrac hrac) {
-        hrac.aktivovatStit(stitEfektivita);
+        hrac.pridatZivoty((int)stitEfektivita);
     }
 }
 
