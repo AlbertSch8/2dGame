25cf651fd2270910282f1f1fdd8a87211a4de6f8
AlbertSch8 <albert.schurrer@gmail.com>
2024-05-29 11:26:03 +0200

    upraveni tridy Batoh

--- src/Batoh.java
+++ src/Batoh.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Batoh extends Herniobjekt{
+public class Batoh extends Herniobjekt {
 
     private static final int MAX_HMOTNOST = 100;
     private int soucasnaHmotnost;
@@ -17,12 +17,10 @@ public class Batoh extends Herniobjekt{
     }
 
     public boolean isJePrazdny() {
-
         return jePrazdny;
     }
 
     public void setJePrazdny(boolean jePrazdny) {
-
         this.jePrazdny = jePrazdny;
     }
 
@@ -33,15 +31,30 @@ public class Batoh extends Herniobjekt{
     public void setJePlny(boolean jePlny) {
         this.jePlny = jePlny;
     }
+
     public void pridejDoBatohu(Veci vec) throws Exception {
         if (soucasnaHmotnost + vec.getHmotnost() > MAX_HMOTNOST) {
             throw new Exception("Přidáním této věci by byl překročen limit hmotnosti batohu.");
-    }
+        }
         batoh.add(vec);
         soucasnaHmotnost += vec.getHmotnost();
         jePrazdny = false;
         jePlny = soucasnaHmotnost >= MAX_HMOTNOST;
     }
+
+    public void odeberZBatohu(Veci vec) {
+        if (batoh.contains(vec)) {
+            batoh.remove(vec);
+            soucasnaHmotnost -= vec.getHmotnost();
+            jePlny = false;
+            jePrazdny = batoh.isEmpty();
+        }
+    }
+
+    public ArrayList<Veci> getBatoh() {
+        return batoh;
+    }
+
     public void vypisObsah() {
         System.out.println("Obsah batohu:");
         for (Veci vec : batoh) {
