2651a7d57f466473aef84ff8814bd098c8abfc10
AlbertSch8 <albert.schurrer@gmail.com>
2024-05-29 11:30:03 +0200

    upraveni tridy LecivyPredmet

--- src/LecivyPredmet.java
+++ src/LecivyPredmet.java
@@ -1,10 +1,9 @@
-public class LecivyPredmet extends Veci{
-
+public class LecivyPredmet extends Veci {
     private int leciveBody;
 
-    public LecivyPredmet(String jmeno, String popis, boolean jeSebratelny, int cisloMistnosti, int hmotnost, int leciveBody) throws Exception {
-        super(jmeno, popis, jeSebratelny, cisloMistnosti, hmotnost);
-        this.leciveBody = leciveBody;
+    public LecivyPredmet(String jmeno, String popis, boolean jeSebratelny, int cisloMistnosti, int hmotnost) throws Exception {
+        super(jmeno, popis, jeSebratelny, cisloMistnosti, hmotnost, 40);
+        this.leciveBody = 40; // Nastavuje léčivé body na 40
     }
 
     public int getLeciveBody() {
